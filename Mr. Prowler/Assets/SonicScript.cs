e85c20cabfa30ab3fcc6207c09b9de3229be4325
Felipe Silva <byonichip@gmail.com>
2023-06-12 21:25:54 -0300

    Física inicial adicionada
    Movimentação e pulos adicionados
    Colisão adicionado

--- Mr. Prowler/Assets/SonicScript.cs
+++ Mr. Prowler/Assets/SonicScript.cs
@@ -2,17 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class SonicScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+    public int maxJumps = 2;
+
+    private Rigidbody2D rb;
+    private bool isJumping = false;
+    private bool isGrounded = false;
+    private int jumpCount = 0;
+
+    private void Awake()
     {
-        gameObject.name = "Sonikku";
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        float moveDirection = Input.GetAxis("Horizontal");
+
+        // Movimento horizontal
+        rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
+
+        // Verificar se o personagem está no chão
+        isGrounded = Physics2D.OverlapCircle(transform.position, groundCheckRadius, groundLayer);
+
+        // Pulo
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+            jumpCount = 1;
+        }
+        else if (!isGrounded && isJumping && jumpCount < maxJumps && Input.GetButtonDown("Jump"))
+        {
+            jumpCount++;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // Aplicar pulo
+        if (isJumping)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            isJumping = false;
+        }
+
+        // Aplicar controle da gravidade
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * 2f * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * 1.5f * Time.deltaTime;
+        }
     }
 }
